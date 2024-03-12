f217de1d3426e7a80af9af7db53d8124d82179ba
秉权 <131164644+jarmWang@users.noreply.github.com>
2024-03-12 15:25:24 +0800

    第三次上传

--- assets/about-35881f13.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as t,c as a,e as s,t as n}from"./index-e09f61fb.js";const r={name:"about",mounted(){console.log(this.$route)}};function c(e,l,u,p,i,_){return t(),a("div",null,[s("p",null,"参数值为: "+n(e.$route),1)])}const f=o(r,[["render",c]]);export{f as default};
