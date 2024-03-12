5f29ac6e4b1ff5ca3a48710b8aa590d72bcdf447
秉权 <131164644+jarmWang@users.noreply.github.com>
2024-03-12 14:36:21 +0800

    第二次上传

---
+++ assets/about-4ec5bed5.js
@@ -0,0 +1,1 @@
+import{_ as o,o as t,c as a,e as s,t as n}from"./index-0aeb8aad.js";const r={name:"about",mounted(){console.log(this.$route)}};function c(e,l,u,p,i,_){return t(),a("div",null,[s("p",null,"参数值为: "+n(e.$route),1)])}const f=o(r,[["render",c]]);export{f as default};
