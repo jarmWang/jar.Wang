3c5b90b629cb9cb4718a3b26a113905f53d098bd
秉权 <131164644+jarmWang@users.noreply.github.com>
2024-03-14 18:07:52 +0800

    提交审批

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,15 +1,32 @@
 <!--
  * @Author: wbq
  * @Date: 2024-03-14 15:19:45
- * @LastEditTime: 2024-03-14 16:37:02
+ * @LastEditTime: 2024-03-14 18:07:17
  * @LastEditors: wbq
  * @Description: 文件功能描述
- * @FilePath: \BaiduSyncdisk\js-vue\demo\vue3\newDemo\new_demo\src\views\index.vue
+ * @FilePath: \BaiduSyncdisk\prod\jar.Wang\src\views\index.vue
 -->
 <template>
     <div class="hemony">
-        <div>
-            
+        <div class="my_container">
+            <el-image
+              class="my_img"
+              :src="state.url"
+              fit="cover"
+            />
+            <div class="my_introduction">
+                <el-card class="my_card" shadow="always">
+                    <div class="my_info space">
+                        <span>姓名：{{ state.formData.name }}</span>
+                    </div>
+                    <div class="my_info space">
+                        <span>性别：{{ state.formData.sex }}</span>
+                    </div>
+                    <div class="my_info space">
+                        <span>年龄：{{ state.formData.age }}</span>
+                    </div>
+                </el-card>
+            </div>
         </div>
     </div>
 </template>
@@ -17,11 +34,43 @@
 <script setup>
 import { reactive } from 'vue'
 const state = reactive({
-    name: 'Home'
+    formData: {
+        name: '王秉权',
+        sex: '男',
+        age: 22
+    },
+    url: require('@/assets/imgs/美女.jpg')
 })
 </script>
 
 <style lang="scss" scoped>
 .hemony {
+    text-align: center;
+    .my_img {
+        margin-top: 10vh;
+        width: 300px;
+        height: 300px;
+        border-radius: 100%;
+        box-shadow: 0 0 10px #000;
+    }
+    .my_introduction {
+        display: grid;
+        justify-items: center;
+        color: #fff;
+
+        .my_card {
+            width: 480px;
+            margin-top: 10px;
+            border: none;
+        }
+
+        .my_info {
+            display: inline-block;
+        }
+
+        .space {
+            margin: 0 10px;
+        }
+    }
 }
 </style>
