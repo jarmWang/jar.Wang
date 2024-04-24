b231254dba99b5722f47c22baec8515b4af36c59
秉权 <131164644+jarmWang@users.noreply.github.com>
2024-04-24 17:53:10 +0800

    4-24

--- src/components/Books/index.vue
+++ src/components/Books/index.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="books">
         <div class="search">
-            <el-input v-model="booksName" placeholder="请输入书籍名称" class="input-with-select">
+            <el-input v-model="booksName" placeholder="请输入完整书名，例如（武动乾坤）" class="input-with-select">
                 <template #append>
-                    <el-button icon="Search" />
+                    <el-button icon="Search" @click="searchClick" />
                 </template>
             </el-input>
         </div>
-        <div>内容显示</div>
+        <div>{{booksList}}</div>
     </div>
 </template>
 
@@ -15,15 +15,14 @@
 import { books } from "./api";
 import { ref, onMounted } from "vue";
 let booksList = ref(null);
-let booksName = ref("完美世界");
+let booksName = ref("武动乾坤");
 onMounted(() => {
     searchClick();
 });
 
 const searchClick = () => {
     books(booksName.value).then((res) => {
-        // booksList.value = res.data;
-        console.log(res);
+        booksList.value = res.data;
     });
 };
 </script>
