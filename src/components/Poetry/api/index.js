a4385090d1bf7e00a9d07dfd777b671088e25a06
秉权 <131164644+jarmWang@users.noreply.github.com>
2024-05-09 14:28:51 +0800

    5-9-5

--- src/components/Poetry/api/index.js
+++ src/components/Poetry/api/index.js
@@ -1,19 +1,21 @@
 /*
  * @Author: wbq
  * @Date: 2024-05-06 10:23:08
- * @LastEditTime: 2024-05-07 09:42:55
+ * @LastEditTime: 2024-05-09 14:26:37
  * @LastEditors: wbq
  * @Description: 文件功能描述
  * @FilePath: \BaiduSyncdisk\prod\jar.Wang\src\components\Poetry\api\index.js
  */
 import { $ajax } from '@/api'
+const url = 'https://api.sou-yun.cn/open/poem'
+
 // 诗词搜索
 export function poetrySearch(data, options = {}) {
     const v = Object.keys(data)
         .map(key => `${key}=${data[key]}`)
         .join('&')
     return $ajax({
-        url: '/poetry?' + v,
+        url: url + '?' + v,
         method: 'get',
         headers: {
             isLoading: true,
